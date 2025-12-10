ef5da1d94edc6ec17037140288851c7a20689ed3
Amaar <amaarulhaq39@gmial.com>
2025-12-10 14:54:13 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var fs = require("fs");
+
+fs.writeFileSync("abc.txt", "hello world");
+
+
+
+
+
+// function fruit(item) {
+//     console.log("Fruit name is " + item);
+// }
+
+// fruit("apple");
+
+// var a = 10;
+// var b = 20;
+// function add(a, b) {
+//     console.log(a + b);
+// }
+
+// add(a, b);
---
+++ basics.js
@@ -0,0 +1,6 @@
+var a = 15;
+let b = 35;
+const c = 55;
+a = 150;
+b = 350;
+console.log(a + b + c);
